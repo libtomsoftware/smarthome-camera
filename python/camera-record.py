a6e190c07e3771317335b5d4bb3124e05956f3d1
Tomasz Libich <t.libich@gmail.com>
2020-07-02 10:57:06 +0100

    WIP python

--- python/camera-record.py
+++ python/camera-record.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python2.7
-from picamera import PiCamera;
-from gpiozero import MotionSensor;
-from time import sleep;
-import time;
-import csv;
+from picamera import PiCamera
+from gpiozero import MotionSensor
+from time import sleep
+import time
+import csv
 
-pir = MotionSensor(4);
-sharedPath = "../shared/";
-configPath = sharedPath + "config/";
-settingsPath = sharedPath + "settings/";
+pir = MotionSensor(4)
+sharedPath = "../shared/"
+configPath = sharedPath + "config/"
+settingsPath = sharedPath + "settings/"
 
-f = open(configPath + "device.txt", "rt");
-device = f.read().replace('\n', '');
-f.close();
+f = open(configPath + "device.txt", "rt")
+device = f.read().replace('\n', '')
+f.close()
 
-timestamp = str(int(time.time()));
-filename = device + "_" + timestamp;
-filename_video = filename + ".h264";
-filename_photo = filename + ".jpg";
-camera = PiCamera();
-camera.rotation = 270;
-camera.resolution = (1920, 1080);
+timestamp = str(int(time.time()))
+filename = device + "_" + timestamp
+filename_video = filename + ".h264"
+filename_photo = filename + ".jpg"
+camera = PiCamera()
+camera.rotation = 270
+camera.resolution = (1920, 1080)
 
-while True:
-	pir.wait_for_motion();
+settingsFile = open(settingsPath + "settings.txt", "rt")
+settings = settingsFile.read().replace('\n', '')
 
-	isArmedFile = open(settingsPath + "is_armed.txt", "rt");
-	is_armed = isArmedFile.read().replace('\n', '');
-		isArmedFile.close();
+print("Settings: %s", settings)
 
-	if is_armed == "1":
-		print("Motion detected, recording!");
+# while True:
+# 	pir.wait_for_motion();
 
-		camera.start_preview();
-		sleep(2);
-		camera.capture(sharedPath + "photo/" + filename_photo);
-		camera.start_recording(sharedPath + "video/" + filename_video);
+# 	isArmedFile = open(settingsPath + "is_armed.txt", "rt");
+# 	is_armed = isArmedFile.read().replace('\n', '');
+# 		isArmedFile.close();
 
-		pir.wait_for_no_motion();
-		print("No more motion, stopping...");
-		camera.stop_recording();
-		camera.stop_preview();
+# 	if is_armed == "1":
+# 		print("Motion detected, recording!");
 
-		f = open(sharedPath + "data/" + timestamp + ".csv", "w");
-		f.write(device + "," + timestamp + "," + filename_photo + "," + filename_video);
-		f.close();
+# 		camera.start_preview();
+# 		sleep(2);
+# 		camera.capture(sharedPath + "photo/" + filename_photo);
+# 		camera.start_recording(sharedPath + "video/" + filename_video);
+
+# 		pir.wait_for_no_motion();
+# 		print("No more motion, stopping...");
+# 		camera.stop_recording();
+# 		camera.stop_preview();
+
+# 		f = open(sharedPath + "data/" + timestamp + ".csv", "w");
+# 		f.write(device + "," + timestamp + "," + filename_photo + "," + filename_video);
+# 		f.close();
--- server/app/monitor.js
+++ server/app/monitor.js
@@ -1,20 +1,22 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const CONFIG = require("./config");
-const URL = CONFIG.SMARTHOME_CENTRAL_URL + "/status";
+const STATUS_URL = CONFIG.SMARTHOME_CENTRAL_URL + "/status";
 
 let isArmedTimeout;
 
-const successTimeout = 5000;
+const successTimeout = 5000; //5 secs
 const failureTimeout = 1800 * 1000; //30 mins
 
 const isArmed = async () => {
   isArmedTimeout = null;
 
   try {
-    const status = await axios.get(URL);
+    const status = await axios.get(STATUS_URL);
+    const { is_armed, is_enabled } = status.data;
 
-    fs.writeFileSync("./shared/data/is_armed.txt", status.data.is_armed);
+    fs.writeFileSync("./shared/data/is_armed.txt", is_armed);
+    fs.writeFileSync("./shared/data/settings.txt", `${is_enabled},${is_armed}`);
 
     isArmedTimeout = setTimeout(isArmed, successTimeout);
   } catch (error) {
